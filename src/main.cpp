2c4991d685bdae4a1cf393deb5c5179ae820c2b3
Jesse <jessemaki@berkeley.edu>
2025-05-06 18:34:50 -0400

    added outputs for angle and bond energy

--- src/main.cpp
+++ src/main.cpp
@@ -29,16 +29,26 @@ int main(int argc, char** argv) {
     // Initialize SPC/E calculator
     SPC spc_calculator(atomic_system.getPositions(), atomic_system.getAtomicNumbers());
 
+    // Display number of water molecules
+    int num_molecules = atomic_system.getNumAtoms() / 3;
+    std::cout << "\n======== System Information ========" << std::endl;
+    std::cout << "Number of atoms: " << atomic_system.getNumAtoms() << std::endl;
+    std::cout << "Number of water molecules: " << num_molecules << std::endl;
+
     // Compute and display energies
     double lj_energy = spc_calculator.getLJEnergy();
     double coulomb_energy = spc_calculator.getCoulombEnergy();
+    double bond_energy = spc_calculator.getBondEnergy();
+    double angle_energy = spc_calculator.getAngleEnergy();
     double total_energy = spc_calculator.getTotalEnergy();
 
     std::cout << std::fixed << std::setprecision(5);
-    std::cout << "\n======== Initial Energy Calculations ========" << std::endl;
+    std::cout << "\n======== Energy Calculations ========" << std::endl;
     std::cout << "Lennard-Jones Energy: " << lj_energy << " Hartree" << std::endl;
     std::cout << "Coulomb Energy: " << coulomb_energy << " Hartree" << std::endl;
-    std::cout << "Total Energy (LJ + Coulomb): " << total_energy << " Hartree" << std::endl;
+    std::cout << "Bond Energy: " << bond_energy << " Hartree" << std::endl;
+    std::cout << "Angle Energy: " << angle_energy << " Hartree" << std::endl;
+    std::cout << "Total Energy: " << total_energy << " Hartree" << std::endl;
     
     std::cout << "\n======== Force Calculation ========" << std::endl;
     CalculateForce force_calculator(spc_calculator, atomic_system.getNumAtoms());
