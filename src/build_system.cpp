08cda8f54dbfeba871768dbac41f4addcf8bffff
Jesse <jessemaki@berkeley.edu>
2025-05-06 18:32:01 -0400

    fixed whitespace

--- src/build_system.cpp
+++ src/build_system.cpp
@@ -24,7 +24,7 @@ void BuildSystem::readFromFile(const std::string& filename) {
         file >> A >> x >> y >> z;
         double x_bohr = x * angstrom_to_bohr;
         double y_bohr = y * angstrom_to_bohr;
-        double z_bohr = z * angstrom_to_bohr;
+        double z_bohr = z * angstrom_to_bohr; 
         std::cout << "Atom " << atom_index << ": A=" << A << ", (x,y,z) in Bohr=(" << x_bohr << "," << y_bohr << "," << z_bohr << ")\n";
         addAtom(Atom(A, x_bohr, y_bohr, z_bohr));
     }
